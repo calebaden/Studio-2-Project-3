8379d4333d71fb2845090f5c94b8181ed3258660
Caleb Middlebrook <1005802@student.sae.edu.au>
2017-03-03 14:47:45 +1000

    Initial project commit

---
+++ Assets/Scripts/DisableFrustumCulling.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableFrustumCulling : MonoBehaviour
+{
+    MeshFilter meshFilter;
+
+	// Use this for initialization
+	void Start ()
+    {
+        meshFilter = GetComponent<MeshFilter>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Transform camTransform = Camera.main.transform;
+        float distToCenter = (Camera.main.farClipPlane - Camera.main.nearClipPlane) / 2.0f;
+        Vector3 center = camTransform.position + camTransform.forward * distToCenter;
+        float extremeBound = 500.0f;
+        meshFilter.sharedMesh.bounds = new Bounds(center, Vector3.one * extremeBound);
+    }
+}
